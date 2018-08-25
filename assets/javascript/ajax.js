5a923eccb740e66e6602522dc3e11f0e9924bf2f
Xavier Allen <xavierallen59@gmail.com>
2018-08-25 00:05:52 -0700

    Played around with the WeatherAPI. Added user input for the location. Need to host own server using Nodejs

---
+++ assets/javascript/ajax.js
@@ -0,0 +1,24 @@
+
+$(document).ready(function(){
+
+
+// jQuery On Click event for adding the city
+$("#cityName").on("click", function (event) {
+    event.preventDefault();
+    // This is the User Input for the city name
+    var userInput = $("#city").val().trim();
+    
+    var apiKey = "39f137c523ad333675cebf77a244c923";
+//Creating the queryURL.
+var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + userInput + "&appid=" + apiKey;
+
+    // Ajax Call for the location
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (resposne) {
+        console.log(resposne);
+    });
+
+});
+});
