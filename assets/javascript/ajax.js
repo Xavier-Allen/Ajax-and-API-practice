e6abbb6a5be9670ff1a39d16c555853523d6b448
unknown <xavierallen@email.arizona.edu>
2018-08-25 19:25:25 -0700

    fixed the API call JS with Herokuapp

--- assets/javascript/ajax.js
+++ assets/javascript/ajax.js
@@ -1,24 +1,25 @@
 
-$(document).ready(function(){
+$(document).ready(function () {
 
 
-// jQuery On Click event for adding the city
-$("#cityName").on("click", function (event) {
-    event.preventDefault();
-    // This is the User Input for the city name
-    var userInput = $("#city").val().trim();
-    
-    var apiKey = "39f137c523ad333675cebf77a244c923";
-//Creating the queryURL.
-var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + userInput + "&appid=" + apiKey;
+    // jQuery On Click event for adding the city
+    $("#cityName").on("click", function (event) {
+        event.preventDefault();
+        // This is the User Input for the city name
+        var userInput = $("#city").val().trim();
 
-    // Ajax Call for the location
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (resposne) {
-        console.log(resposne);
+        var apiKey = "39f137c523ad333675cebf77a244c923";
+        //Creating the queryURL.
+        var queryURL = "https://corsbridge.herokuapp.com/" + 
+            encodeURIComponent("http://api.openweathermap.org/data/2.5/weather?q=" + userInput + "&appid=" + apiKey);
+
+        // Ajax Call for the location
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (resposne) {
+            console.log(resposne);
+        });
+
     });
-
-});
 });
